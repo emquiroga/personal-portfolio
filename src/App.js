ffbd895329b01171bd6b9234d9f869c8ffc09325
Emiliano <emiliano.quiroga093@gmail.com>
2021-08-23 19:02:02 -0300

    third component done. Gonna try particle.js as background

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './components/Home'
 import About from './components/About'
+import Skills from './components/Skills'
 
 function App() {
     return (
         <React.Fragment>    
             <Home />
             <About />
+            <Skills />
         </React.Fragment>   
     )
 };
---
+++ src/components/Skills.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default function Skills() {
+    return (
+        <div className="skills">
+            <h2>CODE SKILLS</h2>
+            <div className="skills-container">
+            <div className="file-name">skills.html</div>
+            <pre>
+                <ul>
+                <li>&lt;html&gt;</li>
+                <li>&lt;head&gt;&lt;/head&gt;</li>
+                <li>&lt;body&gt;</li>
+                <li>&lt;ul&gt;
+                <ul>
+                <li>&lt;li&gt;HTML5&lt;/li&gt;</li>
+                <li>&lt;li&gt;CSS3/Sass&lt;/li&gt;</li>
+                <li>&lt;li&gt;JavaScript&lt;/li&gt;</li>
+                <li>&lt;li&gt;React&lt;/li&gt;</li>
+                <li>&lt;li&gt;npm/webpack&lt;/li&gt;</li>
+                </ul>
+                </li>
+                <li>&lt;/ul&gt;</li>
+                <li>&lt;/body&gt;</li>
+                <li>&lt;/html&gt;</li>
+                </ul>
+            </pre>
+            </div>
+        </div>
+    )
+}
