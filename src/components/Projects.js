50a99db6332e35d542b3046bc32ead1e7ea954c0
Emiliano <emiliano.quiroga093@gmail.com>
2021-09-22 13:13:18 -0300

    started working on the getlinks function

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,7 +9,7 @@ export default function Projects(){
     const toggle = () => {
       setActive(!active);
     }
-
+   
     return(
         <section className="projects">
           <h2>latest pro<span className="special">j</span>ect<span className="special">s</span></h2>
@@ -23,12 +23,6 @@ export default function Projects(){
               <div>
                 <div className="title">Crowfund Landing Page</div>
                 <div className="description">↳  React, Sass, JavaScript, Mobile first workflow</div>
-                <Modal 
-                active={active} 
-                toggle={toggle}
-                repo="https://github.com/emquiroga/crowdfunding-product-page-main/tree/master"
-                live="https://relaxed-noether-77d0b1.netlify.app/"
-                />
               </div>
             </div>
             <div
@@ -39,12 +33,6 @@ export default function Projects(){
               <div>
                 <div className="title">Easybank Landing Page</div>
                 <div className="description">↳  HTML5, Sass, JavaScript, Mobile first workflow</div>
-                <Modal 
-                active={active} 
-                toggle={toggle}
-                repo=""
-                live=""
-                />
               </div>
             </div>
             <div
@@ -55,14 +43,12 @@ export default function Projects(){
            <div>
             <div className="title">Sign-up Form</div>
             <div className="description">↳  HTML5, Sass, JavaScript, Mobile first workflow</div>
-            <Modal 
+           </div>
+           </div>
+           <Modal 
             active={active} 
             toggle={toggle}
-            repo=""
-            live=""
             />
-           </div>
-           </div>
           </div>
         </section>
     )
