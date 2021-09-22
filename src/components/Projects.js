c4a61ba432dafa561affba58e1f0e5bb32985e79
Emiliano <emiliano.quiroga093@gmail.com>
2021-09-22 19:36:24 -0300

    solved the problem for diferent links at project modal. Need to come back to make some dinamic functions instead of repetitive code

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,10 +4,18 @@ import Modal from './Modal';
 
 export default function Projects(){
 
-    const [active, setActive] = useState(false)
+    const [active1, setActive1] = useState(false)
+    const [active2, setActive2] = useState(false)
+    const [active3, setActive3] = useState(false)
 
-    const toggle = () => {
-      setActive(!active);
+    const toggle1 = () => {
+      setActive1(!active1);
+    }
+    const toggle2 = () => {
+      setActive2(!active2);
+    }
+    const toggle3 = () => {
+      setActive3(!active3);
     }
     const links = [
       "https://github.com/emquiroga/crowdfunding-product-page-main",
@@ -17,15 +25,15 @@ export default function Projects(){
       "https://github.com/emquiroga/intro-component-with-signup-form-master",
       "https://emquiroga.github.io/intro-component-with-signup-form-master/"    
       ];
+   
     return(
         <section className="projects">
           <h2>latest pro<span className="special">j</span>ect<span className="special">s</span></h2>
           <div className="projects-wrapper">
             <div
-            type="button"
             className="link-target"
             id="link-1"
-            onClick={toggle}
+            onClick={toggle1}
             >
               <div>
                 <div className="title">Crowfund Landing Page</div>
@@ -35,7 +43,7 @@ export default function Projects(){
             <div
             className="link-target"
             id="link-2"
-            onClick={toggle}
+            onClick={toggle2}
             >
               <div>
                 <div className="title">Easybank Landing Page</div>
@@ -45,7 +53,7 @@ export default function Projects(){
             <div
            className="link-target"
            id="link-3"
-           onClick={toggle}
+           onClick={toggle3}
            >
            <div>
             <div className="title">Sign-up Form</div>
@@ -53,10 +61,22 @@ export default function Projects(){
            </div>
            </div>
            <Modal 
-            active={active} 
-            toggle={toggle}
+            active={active1} 
+            toggle={toggle1}
             repo={links[0]}
             live={links[1]}
+            />
+              <Modal 
+            active={active2} 
+            toggle={toggle2}
+            repo={links[2]}
+            live={links[3]}
+            />
+              <Modal 
+            active={active3} 
+            toggle={toggle3}
+            repo={links[4]}
+            live={links[5]}
             />
           </div>
         </section>
