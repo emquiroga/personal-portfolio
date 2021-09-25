49b4e3c1804ff731617067d996f45f3e2766fd87
Emiliano <emiliano.quiroga093@gmail.com>
2021-09-25 17:46:20 -0300

    text modification

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
             subtitle="frontend developer" />
             <About 
             text="👋 Emi here! Frontend web developer, based on La Plata, Buenos Aires, Argentina.
-            Languages and technology have been always of my attention, so I feel a natural interest about design and code. Creativity and versatility leads my way."
+            Languages, art and technology have been always of my attention, so I feel a natural interest about code and design. Creative, methodical, dynamic."
             img={img}/>
             <WorkAndStudies
             workText="While I'm starting my career as a frontend dev, I'm leaving behind almost ten years as a gastronomic worker and as creative writer. Other jobs included customer support, data entry and language teaching (spanish & latin)." />
